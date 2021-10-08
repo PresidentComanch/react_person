f94f8fb6da402f75cb5019ed3d329e07ab01880b
PresidentComanch <ap.dissembler@gmail.com>
2021-10-08 15:02:06 +0300

    Updates

---
+++ precache-manifest.8a83cecede944999fbb741da2eb4e20a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbee661832d2edb9807893aed0a10eee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee2429e8e90fecb76619",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2429e8e90fecb76619",
+    "url": "./static/js/main.bdf91539.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
